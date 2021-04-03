68e96d864799c47290e883a11c1820360e7c41f7
Baiketli Dijo <baiketlid@gmail.com>
2021-04-03 18:22:22 +0200

    Using in-built logging

--- SerilogDemo/Pages/Index.cshtml.cs
+++ SerilogDemo/Pages/Index.cshtml.cs
@@ -19,7 +19,25 @@ namespace SerilogDemo.Pages
 
         public void OnGet()
         {
+            _logger.LogInformation("Your requested the Index page. ");
 
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i == 56)
+                    {
+                        throw new Exception("This is our demo exception. ");
+                    } else
+                    {
+                        _logger.LogInformation("The value of i is {iVariable}", i);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "We caught this exception in the Index Get call. ");
+            }
         }
     }
 }
